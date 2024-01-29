27dd3b4ccee5693815121523eb8e70a48dd91cd0
Matteo Piccini <gnappo.1@live.it>
2024-01-29 10:00:00 -0800

    prepare base code for lecture 0

---
+++ 00_JS_Fundamentals/index.js
@@ -0,0 +1,25 @@
+// "use strict" // use to enforce a stricter set of rules and best-practices
+
+// JS Fundamentals Part 1
+
+// Single Line Comments 
+/*
+    Multi Line Comments
+*/
+// Toggle comments using cmd + ? (mac) OR cntr + ? (pc)
+// ! Special type of comment!
+
+// Console Logging
+
+// Debugging
+
+// Variables
+
+// Data Types
+
+// Conditional Statements
+
+// Ternary Expressions
+
+// Loops
+

6acb40e95255b613f5d07b87690f35810511fd10
Matteo Piccini <gnappo.1@live.it>
2024-01-30 17:37:00 -0800

    add base code for lecture

---
+++ 02_DOM_Manipulation/src/index.js
@@ -0,0 +1,32 @@
+//* BookStore has been moved to data.js
+console.log(bookStore);
+
+//* Code from previous lecture
+
+function formatPrice(price) {
+  return '$' + Number.parseFloat(price).toFixed(2);
+}
+
+//* New code starts here
+
+//* Concepts
+
+//* querySelector => 
+//* getElementById =>
+//* innerText => 
+//* textContent =>
+//* innerHTML =>
+
+// TODO CRUD
+
+// TODO 💡 Create a function that sets the text content of the header to the bookstore name.
+
+// TODO 💡 Create a function that grabs all the divs from the footer and render the bookstore name, address, and hours
+
+// TODO 💡 Remove an element of your choice from the DOM
+
+// TODO 💡 Exercise After Break
+// create a function called renderBook(book)
+// it will take a book object as an argument
+// and create the html structure for rendering 
+// that book and insert it into our webpage!

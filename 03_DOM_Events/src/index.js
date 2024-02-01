25efb9fbbf77beaf90e092099e3a5b0da437cc15
Matteo Piccini <gnappo.1@live.it>
2024-02-01 12:55:52 -0800

    end of lecture code

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -47,6 +47,9 @@ function renderBook(book) {
     img.src = book.imageUrl
     img.alt = book.title
     const button = document.createElement("button")
+    button.addEventListener('click', () => {
+        li.remove()
+    })
     button.innerText = "Delete"
     li.append(h3, pAuthor, pPrice, img, button)
     // figure out where
@@ -71,7 +74,7 @@ function renderBookAsHTML(book) {
 setHeader()
 changeFooter()
 bookStore.inventory.forEach(bookObj => renderBookAsHTML(bookObj))
-// bookStore.inventory.forEach(renderBook) this line leverages JS magic 
+// bookStore.inventory.forEach(renderBook) //this line leverages JS magic 
 // BUT IT'S IDENTICAL TO THE ONE ABOVE
 
 ////////////////////////////////////////////////////////////////
@@ -80,7 +83,7 @@ bookStore.inventory.forEach(bookObj => renderBookAsHTML(bookObj))
 ////////////////////////////////////////////////////////////////
 //! Generic Syntax For Attaching Event Listeners
 
-// domNodeElement.addEventListener(theEventInStringformat, callbackFunctionThatDesidesWhatToDo, optionalTrueBooleanHere)
+// domNodeElement.addEventListener(theEventInStringformat, callbackFunctionThatDecidesWhatToDo, optionalTrueBooleanHereForCapturing)
 
 //! Suggestions When Working With the DOM
 
@@ -89,11 +92,69 @@ bookStore.inventory.forEach(bookObj => renderBookAsHTML(bookObj))
 //TODO 3. Decide if creating the callback anonymously in-place OR pass a function reference (promotes reusability)
 //TODO 4. Does the callback have access to all the data it needs or should it receive parameters?
 
-
+const toggleBtn = document.querySelector('#toggleForm')
+const form = document.querySelector('#book-form')
+const allDelBtns = document.querySelectorAll('.delete-btn')
 //! Pattern 1: create the function somewhere to promote reusability
-  // then pass the function as a callback to addEventListener
+// then pass the function as a callback to addEventListener
+const handleClick = () => {
+    // somehow remove 'collapsed' from the form if collapsed
+    // somehow add 'collapsed' to the form if NOT collapsed
+    //! I def need a conditional (?)
+    // if (form.className.includes('collapsed')) {
+    //     form.className.replace('collapsed', ' ')
+    // } else {
+    //     form.className += ' collapsed'
+    // }
+
+    //! Using classList I am more efficient and I am not running the risk to override other classnames in there
+    form.classList.toggle('collapsed')
+
+}
+
+const handleSubmit = (e) => {
+    //! PLEASE DO NOT REFRESH THE PAGE, JS WILL HANDLE IT!
+    e.preventDefault()
+    //! Try to extract the values out of the form
+    // const newTitle = document.querySelector('#form-title').value
+    const newTitle = e.target.title.value //! given a form you can access its inputs by name property with .title if camelCase or ['title'] for more flexibility
+    const newAuthor = document.querySelector('#form-author').value
+    const newPrice = document.querySelector('#form-price').value
+    const newImageUrl = document.querySelector('#form-imageUrl').value
+    const newInventory = document.querySelector('#form-inventory').value
+
+    if (!newTitle || !newAuthor) {
+        alert("Title and author must be filled out!")
+        return
+    }
+    //! What do we do with these values?? WE BUILD A NEW BOOK OBJ that matches the default implementation
+    const newBook = {
+        title: newTitle,
+        author: newAuthor,
+        price: newPrice,
+        imageUrl: newImageUrl,
+        inventory: newInventory,
+    }
+
+    //! Time to invoke the fn that displays a book on the page
+    renderBook(newBook)
+
+    //! If the submission worked, EMPTY THE FORM!!!!
+    e.target.reset()
+    // form.reset() equivalent but leveraging the global variable
+
+}
 
 //! Pattern 2: create the callback function in-place, make it anonymous, and 
-  // IF YOU WANT use an arrow function for readability.
+// IF YOU WANT use an arrow function for readability.
+toggleBtn.addEventListener('click', handleClick)
+form.addEventListener('submit', handleSubmit)
+
+
+// allDelBtns.forEach(btn => {
+//     btn.addEventListener('click', (e) => {
+//         e.target.parentElement.remove()
+//     })
+// })
 
 
